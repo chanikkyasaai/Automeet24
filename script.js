57a88928beee21448ab8415d560a5a86a6c2c3e7
Chanikya Sai Nelapatla <125785223+chanikkyasaai@users.noreply.github.com>
2024-03-03 11:13:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+$(window).on("load",function(){
+    $(".loader").fadeOut("slow");
+
+});
+$(document).ready(function () {
+    
+    $(".content").show();
+});
+
+
 const imageSets = [
     ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
     ['5.jpg', '11.jpg', '7.jpg', '8.jpg'],
